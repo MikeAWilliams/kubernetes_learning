5d6e3e6ee6630acf509676cab27088208c39f86c
Mike Williams <mumakin@gmail.com>
2020-03-10 22:59:56 -0600

    add prime factor worker

---
+++ demo_rabbit/factor_worker.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import pika
+import time
+
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='task_queue', durable=True)
+print(' [*] Waiting for messages. To exit press CTRL+C')
+
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+    print(prime_factors(int(body)))
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='task_queue', on_message_callback=callback)
+
+channel.start_consuming()
